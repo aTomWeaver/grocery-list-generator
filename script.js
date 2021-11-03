148c2282ef72a97f1d8e15dd3e40f2a2eeef7415
tomweaver <atomweavermail@gmail.com>
2021-11-03 06:15:15 -0400

    created working pushRecipe function

--- script.js
+++ script.js
@@ -11,14 +11,25 @@ const ingredientFactory = (name, type, isRefrigerated) => {
 const onion = ingredientFactory('onion', 'vegetable', false);
 const groundBeef80 = ingredientFactory('80/20 ground beef', 'meat', true);
 const MexicanCheese = ingredientFactory('Mexican Blend Cheese', 'dairy', true);
+const tacoSauce = ingredientFactory('taco sauce', 'dry', false);
+const tortillas = ingredientFactory('tortillas', 'dry', false);
+const spaghettiNoodles = ingredientFactory('spaghetti noodles', 'dry', false);
+const spaghettiSauce = ingredientFactory('spaghetti sauce', 'dry', false);
 // recipes
-const tacos = recipeFactory('tacos', ['taco sauce', groundBeef80, 'tortillas', MexicanCheese], 'dinner');
-const spaghetti = recipeFactory('spaghetti', ['spaghetti noodles', groundBeef80, 'spaghetti sauce', onion], 'dinner');
+const tacos = recipeFactory('tacos', [tacoSauce, groundBeef80, tortillas, MexicanCheese], 'dinner');
+const spaghetti = recipeFactory('spaghetti', [spaghettiNoodles, groundBeef80, spaghettiSauce, onion], 'dinner');
 
 let recipes = [tacos, ];
 
 
-function pushRecipeToShoppingList() {
+function pushRecipeToShoppingList(obj) {
+    obj.ingredients.forEach(element => {
+        if (shoppingList.includes(element)) {
+            shoppingList[shoppingList.indexOf(element)] += " x2" // need to fix this to include iteratable number
+        } else {
+        shoppingList.push(element.name);
+        }
+    });;
     // get selected recipe from user and return the ingredients list 
     // push ingredients to shoppingList 
 }
