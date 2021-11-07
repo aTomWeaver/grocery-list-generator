8a06078d76ebcd9fe168e4f552be2fdb934e3c23
tomweaver <atomweavermail@gmail.com>
2021-11-07 17:52:17 -0500

    working pushRecipeToShoppingList function

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ const ingredientFactory = (name, type, isRefrigerated) => {
 //ingredients
 const onion = ingredientFactory('onion', 'vegetable', false);
 const groundBeef80 = ingredientFactory('80/20 ground beef', 'meat', true);
-const MexicanCheese = ingredientFactory('Mexican Blend Cheese', 'dairy', true);
+const MexicanCheese = ingredientFactory('mexican blend cheese', 'dairy', true);
 const tacoSauce = ingredientFactory('taco sauce', 'dry', false);
 const tortillas = ingredientFactory('tortillas', 'dry', false);
 const spaghettiNoodles = ingredientFactory('spaghetti noodles', 'dry', false);
 const spaghettiSauce = ingredientFactory('spaghetti sauce', 'dry', false);
+
 // recipes
 const tacos = recipeFactory('tacos', [tacoSauce, groundBeef80, tortillas, MexicanCheese], 'dinner');
 const spaghetti = recipeFactory('spaghetti', [spaghettiNoodles, groundBeef80, spaghettiSauce, onion], 'dinner');
@@ -24,12 +25,18 @@ let recipes = [tacos, ];
 
 function pushRecipeToShoppingList(obj) {
     obj.ingredients.forEach(element => {
-        if (shoppingList.includes(element)) {
-            shoppingList[shoppingList.indexOf(element)] += " x2" // need to fix this to include iteratable number
-        } else {
         shoppingList.push(element.name);
-        }
-    });;
+    });
+    let reducedList = [...new Set(shoppingList)];
+    let qtyArray = [];
+    reducedList.forEach(elm => {
+        let qty = shoppingList.filter(x => x == elm).length;
+        qtyArray.push(qty);
+    })
+    for (i = 0; i < reducedList.length; i++) {
+        reducedList[i] = reducedList[i] + " x" + qtyArray[i]; 
+    }
+    return reducedList;
     // get selected recipe from user and return the ingredients list 
     // push ingredients to shoppingList 
 }
