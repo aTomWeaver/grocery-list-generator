ed408fb54b3a48397e398862937a8337c17bacd9
atomweaver <atomweavermail@gmail.com>
2022-01-17 18:12:27 -0500

    add grocery/recipe adding functionality

--- modularDesign.js
+++ modularDesign.js
@@ -3,17 +3,18 @@ const local = window.localStorage;
 // recipes start
 local.setItem('tacos', JSON.stringify(['taco sauce', '1 lb 80/20 ground beef', 'tortillas', 'mexican blend cheese']));
 local.setItem('spaghetti', JSON.stringify(['spaghetti noodles', '1 lb 80/20 ground beef', 'spaghetti sauce', 'yellow onion']));
-local.setItem('jambalaya', JSON.stringify(['andouille sausage', 'chicken thighs', 'chicken broth','chicken broth',
+local.setItem('jambalaya', JSON.stringify(['andouille sausage', 'chicken thighs', 'chicken broth', 'chicken broth',
     'crushed tomatoes', 'yellow onion', '? minced garlic']));
 local.setItem('sandwiches', JSON.stringify(['bread', 'deli meat', 'deli meat', 'sandwich cheese', '? mayo']));
 local.setItem('chicken pasta', JSON.stringify(['rotisserie', '? rotini', '? pesto', '? tomato sauce', '? parmesan cheese']));
 local.setItem('chicken wraps', JSON.stringify(['tortillas', 'romaine', 'feta', 'rotisserie', '? ginger dressing', '? black olives']));
 local.setItem('chicken teriyaki', JSON.stringify(['chicken thighs', 'broccoli', '? rice', 'sake', 'mirin', '? sugar', 'soy sauce']));
-local.setItem('chili', JSON.stringify(['1 lb 80/20 ground beef', 'diced tomatoes', 'diced tomatoes', 'chicken broth', 
+local.setItem('chili', JSON.stringify(['1 lb 80/20 ground beef', 'diced tomatoes', 'diced tomatoes', 'chicken broth',
     'dark kidney beans', 'black beans', '? chili powder', 'yellow onion', '? red pepper flakes']));
 // recipes end
 
 const recipes = (() => {
+    // this.addBuffer = 0;
     const _init = () => {
         _cacheDOM();
         _bindEvents();
@@ -25,7 +26,7 @@ const recipes = (() => {
     const _bindEvents = () => {
 
     }
-    const _localToArr = () => {
+    const localToArr = () => {
         let arr = [];
         for (i = 0; i < local.length; i++) {
             arr.push(JSON.parse(local.getItem(local.key(i))));
@@ -35,13 +36,12 @@ const recipes = (() => {
     const _render = () => {
         for (i = 0; i < local.length; i++) {
             const recipeCard = document.createElement('div');
-            
             recipeCard.classList.add('recipe');
             recipeCard.setAttribute('data-listNum', i);
             recipeCard.innerText = local.key(i);
-            recipeCard.addEventListener('click', e => {
-              console.log(e.target.dataset.listnum)
-            })
+            // recipeCard.addEventListener('click', e => {
+            //     addBuffer = e.target.dataset.listnum;
+            // })
             this.recipeContainer.append(recipeCard);
         }
     }
@@ -52,9 +52,9 @@ const recipes = (() => {
     const rmvRecipe = () => {
         // local.removeItem()
     }
-    let list = _localToArr();
+    let list = localToArr();
     _init();
-    return { list, refreshOptions, _localToArr }
+    return { list, refreshOptions, localToArr }
 })();
 
 
@@ -89,15 +89,16 @@ const modal = (() => {
         _close();
         local.setItem(name, JSON.stringify(ingredients));
         recipes.refreshOptions();
-        recipes.list = recipes._localToArr();
+        recipes.list = recipes.localToArr();
     }
     _init();
 })();
 
 
-
+// the rendering of this grocery list is smelly; I'll fix it up later
 const groceryList = (() => {
     let items = [];
+    let listItems;
     const _init = () => {
         _cacheDOM();
         _bindEvents();
@@ -105,18 +106,48 @@ const groceryList = (() => {
     }
     const _cacheDOM = () => {
         this.options = document.querySelectorAll('.recipe');
+        this.listContainer = document.querySelector('.grocery-list-container')
+        this.recipesAddedContainer = document.querySelector('.recipes-added-container');
     }
     const _bindEvents = () => {
-        
+        options.forEach(element => {
+            element.addEventListener('click', e => addRecipe(e.target.dataset.listnum));
+        });
+    }
+    const _renderRecipeList = () => { 
+        for (let i = 0; i < listItems.length; i++) {
+            // render recipes-added 
+            const recipeCard = document.createElement('div');
+            recipeCard.innerText = listItems[i];
+            this.recipesAddedContainer.append(recipeCard);
+        }
+    }
+    const _renderIngredientList = () => {
+        for (let i = 0; i < listItems.length; i++) {
+            const groceryCard = document.createElement('div');
+            const recipeArray = JSON.parse(local[local.key(i)]);
+            recipeArray.forEach(elm => {
+                const ingredientCard = document.createElement('div');
+                ingredientCard.innerText = elm;
+                this.listContainer.append(ingredientCard);
+            });
+        }
     }
     const _render = () => {
-
+        listItems = items.map(elm => local.key(elm));
+        this.recipesAddedContainer.innerHTML = '';
+        this.listContainer.innerHTML = '';
+        _renderRecipeList();
+        _renderIngredientList();
     }
-    const addRecipe = () => {
-
+    const addRecipe = (recipeNum) => {
+        items.push(parseInt(recipeNum));
+        _render();
     }
     const addSingleItem = () => {
-
+        const item = prompt('Single item: ');
+        listContainer.append(item);
     }
     _init();
+    return { addRecipe, items }
 })();
